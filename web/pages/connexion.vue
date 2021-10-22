397b8f1188838574779312f4eda3e80ae0df9ce3
ChristianoDc7 <christianod97@gmail.com>
2021-10-22 15:48:57 +0300

    feat login signup

--- web/pages/connexion.vue
+++ web/pages/connexion.vue
@@ -1,31 +1,191 @@
 <template>
-  <div>
-    <form id="whoobe-t7qyk" class="justify-center items-center w-full shadow rounded-lg bg-white px-6 flex flex-col md:w-1/2 lg:w-1/3 m-auto">
-    <h2 class="text-2xl my-4">Login</h2>
-			<div id="whoobe-h90kl" class="w-full p-2 justify-start flex flex-col">
-				<div id="whoobe-7izhv" class=" flex flex-row">
-					<span id="whoobe-plfl9" class="z-highest rounded-l-lg w-10 h-10 flex justify-center items-center text-2xl text-gray-400 border border-r-0" mode="render" block="">
-						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 26 26" class="iconify iconify--wpf">
-							<path d="M16.563 15.9c-.159-.052-1.164-.505-.536-2.414h-.009c1.637-1.686 2.888-4.399 2.888-7.07c0-4.107-2.731-6.26-5.905-6.26c-3.176 0-5.892 2.152-5.892 6.26c0 2.682 1.244 5.406 2.891 7.088c.642 1.684-.506 2.309-.746 2.397c-3.324 1.202-7.224 3.393-7.224 5.556v.811c0 2.947 5.714 3.617 11.002 3.617c5.296 0 10.938-.67 10.938-3.617v-.811c0-2.228-3.919-4.402-7.407-5.557z" fill="currentColor">
-							</path>
-						</svg>
-					</span>
-					<input value="" class="border border-gray-200 rounded-r-lg outline-none focus:ring-1 ring-blue-400 w-full pl-1" id="" name="" required="false" placeholder="username">
-				</div>
+  <div class="max-w-full">
+    	<div class="min-w-full justify-center " :class="oks? 'hidden' : 'block'">
+			<div class="flex justify-center">
+				<!-- Row -->
 				
-				<div id="whoobe-l6k6r" class="my-4 flex flex-row">
-					<span id="whoobe-4occ6" class="z-highest rounded-l-lg w-10 h-10 flex justify-center items-center text-2xl text-gray-400 border border-r-0" mode="render" block="">
-						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32" class="iconify iconify--carbon">
-							<path d="M21 2a8.998 8.998 0 0 0-8.612 11.612L2 24v6h6l10.388-10.388A9 9 0 1 0 21 2zm0 16a7.013 7.013 0 0 1-2.032-.302l-1.147-.348l-.847.847l-3.181 3.181L12.414 20L11 21.414l1.379 1.379l-1.586 1.586L9.414 23L8 24.414l1.379 1.379L7.172 28H4v-3.172l9.802-9.802l.848-.847l-.348-1.147A7 7 0 1 1 21 18z" fill="currentColor">
-							</path>
-							<circle cx="22" cy="10" r="2" fill="currentColor">
-							</circle>
-						</svg>
-					</span>
-					<input type="password" value="" class="h-10 border border-gray-200 rounded-r-lg outline-none focus:ring-1 ring-blue-300 w-full pl-1" id="" name="" placeholder="password" required="false">
+					<!-- Col -->
+					<div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+						<h3 class="cursor-pointer pt-4 text-2xl text-center" v-on:click="sat">Create an Account!</h3>
+            <h3 class="cursor-pointer pt-4 text-2xl text-center text-blue-500" v-on:click="sot">Login</h3>
+
+						<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+							<div class="mb-4">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+									Email or username
+								</label>
+								<input
+									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="email"
+									type="email"
+									placeholder="Email"
+								/>
+							</div>
+							
+								<div class="mb-4">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+									Password
+								</label>
+								<input
+									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="email"
+									type="password"
+									placeholder="***************"
+								/>
+							</div>
+								
+					
+							<div class="mb-6 text-center">
+								<button
+									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+									type="button"
+								>
+									Login
+								</button>
+							</div>
+							<hr class="mb-6 border-t" />
+							<div class="text-center">
+								<a
+									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									href="#"
+								>
+									Forgot Password?
+								</a>
+							</div>
+							<div class="text-center">
+								<a
+									class="cursor-pointer inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									 v-on:click="sot"
+								>
+									Already have an account? Login!
+								</a>
+							</div>
+						</form>
+					
 				</div>
-				<button value="button" class="px-4 py-2 rounded bg-blue-400 text-white hover:bg-blue-700 my-4 w-full" id="whoobe-ibemp">Login</button>
 			</div>
-		</form>
+		</div>
+ 
+
+		<div class="container min-w-full justify-center" :class="oks? 'block' : 'hidden'">
+			<div class="flex justify-center">
+				<!-- Row -->
+			
+					<!-- Col -->
+					<div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+						<h3 class="cursor-pointer pt-4 text-2xl text-center text-blue-500" v-on:click="sat">Create an Account!</h3>
+            <h3 class="cursor-pointer pt-4 text-2xl text-center" v-on:click="sot">Login</h3>
+
+						<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+							<div class="mb-4 md:flex md:justify-between">
+								<div class="mb-4 md:mr-2 md:mb-0">
+									<label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
+										First Name
+									</label>
+									<input
+										class="w-full pr-20 pl-2 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="firstName"
+										type="text"
+										placeholder="First Name"
+									/>
+								</div>
+								<div class="md:ml-2">
+									<label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
+										Last Name
+									</label>
+									<input
+										class="w-full pr-20 pl-2 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="lastName"
+										type="text"
+										placeholder="Last Name"
+									/>
+								</div>
+							</div>
+							<div class="mb-4">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+									Email
+								</label>
+								<input
+									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="email"
+									type="email"
+									placeholder="Email"
+								/>
+							</div>
+							<div class="mb-4 md:flex md:justify-between">
+								<div class="mb-4 md:mr-2 md:mb-0">
+									<label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+										Password
+									</label>
+									<input
+										class="w-full pr-20 pl-2 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="password"
+										type="password"
+										placeholder="******************"
+									/>
+								
+								</div>
+								<div class="md:ml-2">
+									<label class="block mb-2 text-sm font-bold text-gray-700" for="c_password">
+										Confirm Password
+									</label>
+									<input
+										class="w-full pr-20 pl-2 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="c_password"
+										type="password"
+										placeholder="******************"
+									/>
+								</div>
+							</div>
+							<div class="mb-6 text-center">
+								<button
+									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+									type="button"
+								>
+									Register Account
+								</button>
+							</div>
+							<hr class="mb-6 border-t" />
+							<div class="text-center">
+								<a
+									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									href="#"
+								>
+									Forgot Password?
+								</a>
+							</div>
+							<div class="text-center">
+								<a
+									class="cursor-pointer inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+									 v-on:click="sot"
+								>
+									Already have an account? Login!
+								</a>
+							</div>
+						</form>
+					
+				</div>
+			</div>
+		</div>
+
   </div>
 </template>
+
+<script>
+export default {
+  name : 'connexion',
+  data: () => ({
+    message: "bonjour le monde",
+    nom: "Christiano",
+    oks: false
+  }),
+  methods : {
+    sat : function(){
+      this.oks = true
+    },
+    sot : function(){
+      this.oks = false
+    }
+  }
+}
+</script>
