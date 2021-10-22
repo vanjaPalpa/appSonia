f0ec6e0787f50b4c391bc3df9e14466fb7dfcf3c
AinaTahina <ainatahinasafidy@gmail.com>
2021-10-22 11:07:54 +0200

    feat: navbar

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <nav>
-      menu
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/connexion">Connexion</a></li>
-        <li><a href="/checkID">checkID</a></li>
+    <nav class="w-full bg-red-500 items-center">
+      <ul class="text-base bg-gray-900 text-gray-100 flex flex-row  overflow-hidden">
+        <li class="m-4 text-xl text-red-400">APPsonia</li>
+        <div class="flex flex-row float-right">
+          <li class="m-4"><a href="/">Home</a></li>
+          <li class="m-4"><a href="/connexion">Connexion</a></li>
+          <li class="m-4"><a href="/checkID">checkID</a></li>
+          </div>
       </ul>
     </nav>
     <div>
