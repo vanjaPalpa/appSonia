5ca15c501a18d631dd127abeae5e16f1970c7bd9
Jennica Randriamanalina <jennicarandriamanalina@gmail.com>
2021-10-22 13:03:08 +0300

    feat(home): add contents in the hero page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <h1>home</h1>
+  <div class="p-8">
+    <div>
+      <h2>Description du projet</h2>
+      <p class="text-gray-500"> Application créant un Identité unique des citoyens tout en visant à certifier les documents d'état civil par un sceau digital et une signature numérique.
+        l'application garantit la vérification, authenticité des citoyens et des documents d'état civil 
+       </p>
+    </div>
+    <div>
+      <h2>Aide d'utilisation de cette application</h2>
+      <p class="text-gray-500">
+        Tout d'abord, les utilisateurs doivent s'inscrire: </br>
+       </p>
+          <p class="p-3 text-gray-500"> 
+            * Remplir les formulaires </br>
+            * Sauvegarder et envoyer toutes les informations  </br>
+          </p>
+        <p class="text-gray-500">
+             Puis veuillez attendre jusqu'à ce que l'administrateur approuve toutes les informations que vous avez saisir </br>
+
+        Enfin, vous pouvez telecharger les documents
+
+        </p>
+     
+
+     
+
+    </div>
   </div>
 </template>
 
