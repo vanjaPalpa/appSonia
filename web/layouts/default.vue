f98c3330f5613de9b5be5197e25949cd4ac97f40
AinaTahina <ainatahinasafidy@gmail.com>
2021-10-22 13:00:37 +0200

    feat: navbardesktop

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <nav class="w-full bg-red-500 items-center ">
-      <ul class="text-base bg-gray-900 text-gray-100 flex flex-col  md:flex-row justify-between pr-4 pf-4">
-        <div class="flex flex-row space justify-between items-center">
-          <h3 class="m-4 text-xl text-red-400  ">APPsonia</h3>
+    <nav class="w-full bg-red-500 ">
+      <ul class="text-base bg-gray-900 text-gray-100 flex flex-col pr-4 pl-4  md:flex-row md:justify-between md:pr-12 md:pl-12">
+        <div class="flex flex-row space justify-between items-center md:inline-block ">
+          <h3 class="m-4 text-2xl font-semibold text-red-400  md:ml-0">APPsonia</h3>
           <span class="m-4 md:hidden">X</span>
         </div>
         <div class="flex flex-col md:flex-row">
