f99a476ad027f0a2057a6d278588676be0c76252
Jennica Randriamanalina <jennicarandriamanalina@gmail.com>
2021-10-22 13:52:33 +0300

    feat(Admin): add new file contentAdmin.vue and add cntents in this file

--- web/pages/admin.vue
+++ web/pages/admin.vue
@@ -42,7 +42,9 @@
 </aside>
 
 <section class="w-full p-4">
-  <div class="w-full h-64 border-dashed border-4 p-4 text-md">Dashboard</div>
+  <div class="w-full h-64   text-md">
+    <content-admin></content-admin>
+  </div>
   </section>
 
 </main> 
