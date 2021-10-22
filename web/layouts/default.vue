8f3ba3619ef27905effd3fff8da44ca25586b308
AinaTahina <ainatahinasafidy@gmail.com>
2021-10-22 11:29:27 +0200

    feat: navbarMobil

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <nav class="w-full bg-red-500 items-center">
-      <ul class="text-base bg-gray-900 text-gray-100 flex flex-row  overflow-hidden">
-        <li class="m-4 text-xl text-red-400">APPsonia</li>
-        <div class="flex flex-row float-right">
+    <nav class="w-full bg-red-500 items-center ">
+      <ul class="text-base bg-gray-900 text-gray-100 flex flex-col">
+        <div class="flex flex-row space justify-between items-center">
+          <h3 class="m-4 text-xl text-red-400">APPsonia</h3>
+          <span class="m-4">X</span>
+        </div>
+        <div class="flex flex-col">
           <li class="m-4"><a href="/">Home</a></li>
           <li class="m-4"><a href="/connexion">Connexion</a></li>
           <li class="m-4"><a href="/checkID">checkID</a></li>
-          </div>
+        </div>
       </ul>
     </nav>
     <div>
