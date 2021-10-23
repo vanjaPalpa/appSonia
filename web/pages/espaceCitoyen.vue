ceee480ce7bd86e10b17b5e4680e3937b0b9f12c
Fanaingo <fanaingorakotomalala@gmail.com>
2021-10-23 11:31:06 +0300

    add formulaire on the first section in profil.vue / components

--- web/pages/espaceCitoyen.vue
+++ web/pages/espaceCitoyen.vue
@@ -33,7 +33,7 @@
           </div>
         </aside>
         <section class="w-full p-4">
-          <div class="w-full h-64 border-dashed border-4 p-4 text-md">Dashboard</div>
+          <div class="w-full h-64 border-dashed border-4 p-4 text-md"><profil></profil></div>
           </section>
       </main>
   
