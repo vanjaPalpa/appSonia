a5bb6c6ff2e755f3946ad065a3a9f03686c5b4c6
ChristianoDc7 <christianod97@gmail.com>
2021-10-23 17:07:08 +0300

    farany

--- web/components/profil.vue
+++ web/components/profil.vue
@@ -3,7 +3,6 @@
 		<!-- front profil -->
 		<section>
 			<div>
-				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 				<div class="border-green-600 border-2">
 					<div class="flex items-center justify-center ">
 						<img src="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" class="rounded-full w-32">
