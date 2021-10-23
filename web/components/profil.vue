7fb3e3dd90cc5bc424a07d28f148d45371d52b5b
Fanaingo <fanaingorakotomalala@gmail.com>
2021-10-23 14:14:27 +0300

    remove v-if attributes into main on espaceCitoyen.vue && complete all formulaire on profil.vue

--- web/components/profil.vue
+++ web/components/profil.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
 		
-		<!-- front profil -->
 		<section>
 			<form>
 				<div class="mb-4">
@@ -25,7 +24,11 @@
 						type="text"
 					/>
 				</div>
+			</form>
+		</section>
 
+		<section>
+			<form>
 				<div class="mb-4">
 					<label class="block mb-2 text-sm font-bold text-gray-700" for="birth">
 						TERAKA TAMIN' NY / Né(e) le:
@@ -60,6 +63,84 @@
 				</div>
 
 				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="legnth">
+						TAILLE / Refy:  
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="legnth"
+						type="text"
+					/>
+				</div>
+			</form>
+		</section>
+		
+		<section>
+			<form>
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="adress">
+						FONENANANA / Adresse:
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="adress"
+						type="text"
+					/>
+				</div>
+
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="fathername">
+						BORIBORINTANY / Arrondissement:
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="fathername"
+						type="text"
+					/>
+				</div>
+
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="job">
+						ASA ATAO / Profession:
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="job"
+						type="text"
+					/>
+				</div>
+			</form>
+		</section>
+
+		<section>
+			<form>
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="fathername">
+						RAY NITERAKA / Père
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="fathername"
+						type="text"
+					/>
+				</div>
+
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="mothername">
+						RENY NITERAKA / Mère:
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="mothername"
+						type="text"
+					/>
+				</div>
+			</form>
+		</section>
+
+		<section>
+			<form>
+				<div class="mb-4">
 					<label class="block mb-2 text-sm font-bold text-gray-700" for="cin">
 						LAHARANA KARAPANONDRO / Numéro CIN:
 					</label>
@@ -68,12 +149,31 @@
 						id="cin"
 						type="text"
 					/>
+				</div>	
+
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="placeCin">
+						NATAO TAO / Fait à: 
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="placeCin"
+						type="text"
+					/>
+				</div>
+
+				<div class="mb-4">
+					<label class="block mb-2 text-sm font-bold text-gray-700" for="dateCin">
+						TAMIN' NY / Le:
+					</label>
+					<input
+						class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+						id="dateCin"
+						type="text"
+					/>
 				</div>
 			</form>
 		</section>
-
-		<!-- back profil -->
-		<section></section>
 
 	</div>
 </template>
