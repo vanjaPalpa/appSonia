612f5e9e61e34fdab58eba8876d7989145365ca1
ChristianoDc7 <christianod97@gmail.com>
2021-10-24 10:24:33 +0300

    ayooo design

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,14 +1,20 @@
+<style>
+.bgim{
+  background: linear-gradient(100deg, rgb(224, 70, 70) 0% ,rgb(255, 255, 255) 90%);
+}
+</style>
 <template>
-  <div class="pl-28 pr-28 ">
+<body class="bgim">
+  <div class="pl-20">
     <div class="mb-14 mt-8">
-      <h1 class="text-2xl font-semibold text-gray-700">Description du projet</h1>
+      <h1 class="text-4xl font-black text-gray-700">Description du projet</h1>
       <p class="ml-7 text-gray-900"> Application créant un Identité unique des citoyens tout en visant à certifier les documents d'état civil par un sceau digital et une signature numérique.
         l'application garantit la vérification, authenticité des citoyens et des documents d'état civil 
       </p>
     </div>
 
     <div class="">
-      <h2  class="text-2xl font-semibold text-gray-700">Aide d'utilisation de cette application</h2>
+      <h1  class="text-4xl font-black text-gray-700">Aide d'utilisation de cette application</h1>
 
       <p class="ml-7 text-gray-900">
         Tout d'abord, les utilisateurs doivent s'inscrire:<br>
@@ -25,6 +31,7 @@
       </p>
     </div>
   </div>
+</body>
 </template>
 
 <script>
